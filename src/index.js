2baf82f6307506443444e3f00992545f00f00734
“webproject67” <webproject67@gmail.com>
2020-11-06 16:57:32 +0300

    add main

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import './sass/style.scss';
 
 const svgModules = require.context('../src/img/svg', true, /\.svg$/);
 svgModules.keys().forEach(svgModules);
+
+const sliders = document.querySelectorAll(`.sliders__item`);
+const btns = document.querySelectorAll(`.sliders__btn`);
+
+btns.forEach((btn, i) => {
+  const index = i;
+  btn.addEventListener('click', () => {
+    for(const btn of btns) btn.classList.remove('sliders__btn--active');
+    btn.classList.add('sliders__btn--active');
+    for(const slider of sliders) slider.classList.remove('sliders__item--active');
+    sliders[i].classList.add('sliders__item--active');
+  })
+})
+
